799758bd2a8a16ab6e317bb50ab692d8766f78b1
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-04-25 18:12:23 -0400

    answered all exercises from 1 to 6 + strech

--- workshop/6-string-methods/6-exercise-1-to-6.js
+++ workshop/6-string-methods/6-exercise-1-to-6.js
@@ -51,40 +51,54 @@ console.log(`A1) Morty has ${numberOfNames} names`);
 // Convert the alternateRicks string to an array, so that each name ("Simple
 // Rick") is an element in that array. Log that array to the console.
 
-//
-//
-//
-//
+newRicks = alternateRicks.split(", ");
+console.log(newRicks);
+
+
+
 // Q3
 // How many Ricks have been named in alternateRicks?
 // HINT: There are multiple ways to solve this one. Maybe try a `for` loop!
 
-//
-//
-//
-//
+console.log(`There are ${newRicks.length} Ricks`);
+
+//different attempt with for loops
+for(let i=0; rick < newRicks.length; i++) {
+  let count = 0;
+  if (newRicks[i].includes("Rick")) {
+    count++;
+  }
+}
+console.log(count);
+
 // Q4
 // How many characters are there in rickSaying (without spaces)?
 
-//
-//
-//
-//
+newRickSaying = rickSaying.split(" ").join("");
+console.log(newRickSaying);
+
 // Q5
 // It turns out, Morty doesn't actually have a middle name. The `grandson`
 // string is wrong! Console Morty's name without "Antoine".
 
-//
-//
-//
-//
+newGrandson = grandson.replace("Antoine ","");
+console.log(newGrandson);
+
 // Q6
 // Scary Terry is actually not an enemy. Remove him from the list and console only Rick's true enemies.
+trueEnemies = enemies.replace(", Scary Terry", "");
+console.log(enemies);
+console.log(trueEnemies);
 
-//
-//
-//
-//
+//or
+console.log(enemies.split(", ").indexOf("Scary Terry"));
+//returns 6th position
+enemies_split = enemies.split(", "); // splits list into array
+enemies_split.splice(6,1); // removes 6th element, which is scary terry, but returns scarry terry
+console.log(enemies_split); //actual list of enemies
+
+
+
 // Q7 (STRETCH)
 // `secret` is an encrypted message! Let's see if we can figure out what it
 // says. To decode the message, you'll need to remove any words that are found
@@ -94,3 +108,62 @@ console.log(`A1) Morty has ${numberOfNames} names`);
 //   IF secret was `bluehired`
 //   AND notCode was `['blue', 'red']`
 //   THEN the answer would be `hi` (bluehired without blue and red).
+
+
+// ####################################################################################################
+// ######################################## SOLUTION ##################################################
+// ####################################################################################################
+
+const secret =
+  "summerlobaconokfamily ascaryt mintergalactice, I'jerrym Pimortybethckle Rick!";
+const notCode = [
+  'summer',
+  'bacon',
+  'scary',
+  'intergalactic',
+  'jerry',
+  'morty',
+  'beth',
+  'family',
+];
+
+
+//strings are immutable, need to convert to array
+let arr = secret.split(" ");
+
+//first loop through each element in the encrypted array
+for (let i=0; i < arr.length; i++) {
+
+  //second loop through each encryption word in each encrypted element
+  for (let j=0; j < notCode.length; j++ ) {
+
+    // check if the encryption word can be found in the encrypted element
+    if(arr[i].includes(notCode[j])) {
+
+      //replace the encrypted word by a blank, and re-assign it to the element
+      arr[i] = arr[i].replace(`${notCode[j]}`,``);
+      // console.log(arr[i]);
+    }
+  }
+}
+
+decodedMessage = arr.join(" ")
+console.log(decodedMessage);
+
+
+//some stuff i was working on...
+
+//loop through each item in the array
+for (let i=0; i < arr.length; i++) {
+  //for each encryped word, check if the code words are included, if they are then remove
+  for (let j=0; j < notCode.length; j++ ) {
+    if(arr[i].includes(notCode[j])) {
+      // console.log("Theres a codeword here!");
+      let notCodeIndex;
+      let notCodeLength;
+      notCodeIndex = arr[i].indexOf(`${notCode[j]}`);
+      notCodeLength = notCode[j].length + notCodeIndex;
+      console.log(`Found encryption word ${notCode[j]} in ${arr[i]} which starts at ${notCodeIndex} and has length of ${notCodeLength} characters`);
+    }
+  }
+}
