7bc878c8523768882dd0ea8ccb677f8140807446
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-04-25 18:15:00 -0400

    completed 8-1

--- workshop/8-functions/8-exercise-1.js
+++ workshop/8-functions/8-exercise-1.js
@@ -7,3 +7,15 @@ function max(num1, num2) {}
 
 // Call the function
 max(12, 43);
+
+
+// Solution
+
+function max(a,b) {
+    if (a > b ){
+        console.log(`${a} is the bigger number`);
+    } else {
+        console.log(`${b} is the bigger number`);
+    }
+}
+max(12, 43);
